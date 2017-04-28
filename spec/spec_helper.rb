985ab3fa230871a8d99a48420d7b4bf6cb9a58be
christopher hutchins <chris.s.hutchins@gmail.com>
2017-04-28 10:57:57 -0700

    ADD capybara config to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,7 @@
 require 'capybara/rspec'
 require 'selenium-webdriver'
 require 'httparty'
+
+Capybara.run_server = false
+Capybara.default_driver = :selenium
+Capybara.app_host = 'http://patient.heal.com'
